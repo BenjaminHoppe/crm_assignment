992e0094649bbb82179642966a5adb4b84da140e
Benjamin <hoppeconnect@gmail.com>
2018-11-26 08:42:07 -0500

    still need to add a few more things (I think)

--- contact.rb
+++ contact.rb
@@ -1,58 +1,112 @@
 class Contact
 
-  # This method should initialize the contact's attributes
-  def initialize
+  @@contacts = []
+  @@id = 1
 
+  def initialize(first_name, last_name, email, note)
+      @first_name = first_name
+      @last_name = last_name
+      @email = email
+      @note = note
+    end
+
+  def self.create(first_name, last_name, email, note)
+    new_contact = Contact.new(first_name, last_name, email, note)
+    @@contacts << new_contact
+    return new_contact
   end
 
-  # This method should call the initializer, 
-  # store the newly created contact, and then return it
-  def self.create
 
-  end
-
-  # This method should return all of the existing contacts
   def self.all
-
+    return @@contacts
   end
 
-  # This method should accept an id as an argument
-  # and return the contact who has that id
-  def self.find
-
+  def self.find(id)
+    @@contacts.each do | contact |
+      if contact.id == id
+        return contact
+      end
+    end
   end
 
-  # This method should allow you to specify 
-  # 1. which of the contact's attributes you want to update
-  # 2. the new value for that attribute
-  # and then make the appropriate change to the contact
-  def update
-
+  def self.find_by(attribute, value)
+    if attribute == "first_name"
+      return @@contacts.select{|contact| contact.first_name == value}
+    end
+    if attribute == "last_name"
+      return @@contacts.select{|contact| contact.last_name == value}
+    end
+    if attribute == "email"
+      return @@contacts.select{|contact| contact.email == value}
+    end
+    if attribute == "note"
+      return @@contacts.select{|contact| contact.note == value}
   end
+end
 
-  # This method should work similarly to the find method above
-  # but it should allow you to search for a contact using attributes other than id
-  # by specifying both the name of the attribute and the value
-  # eg. searching for 'first_name', 'Betty' should return the first contact named Betty
-  def self.find_by
-
-  end
-
-  # This method should delete all of the contacts
   def self.delete_all
+    @@contacts = []
+  end
 
+  # Instance Methods ---------------------------
+
+  def update(attribute, value)
+    attribute = value
   end
 
   def full_name
-
+    return "#{first_name} #{last_name}"
   end
 
-  # This method should delete the contact
-  # HINT: Check the Array class docs for built-in methods that might be useful here
   def delete
-
+    @@contact.delete(self)
   end
 
-  # Feel free to add other methods here, if you need them.
-  
+  # Readers for arrguments in ititialize
+  def first_name
+    return @first_name
+  end
+
+  def last_name
+    return @last_name
+  end
+
+  def email
+    return @email
+  end
+
+  def note
+    return @note
+  end
+  def contacts
+    return @@contacts
+  end
+
+  # Writers for arrguments in ititialize
+
+  def first_name=(first_name)
+    @first_name = first_name
+  end
+
+  def last_name=(last_name)
+    @last_name = last_name
+  end
+
+  def email=(email)
+    @email = email
+  end
+
+  def note=(note)
+    @note = note
+  end
+
 end
+
+ben = Contact.create("Benjamin", "Hoppe", "benjamin@benjaminhoppe.co", "obsessed with dark mode")
+
+oliur = Contact.create("Oliur", "Rahman", "oliur@me.com", "is a great designer.")
+
+puts "#{ben.full_name}'s email is #{ben.email}, and something you should know about him is that he's #{ben.note}."
+puts "------------------------------------"
+puts "you can reach #{oliur.full_name} at #{oliur.email}, and something you should know about him is that he #{oliur.note}"
+puts "------------------------------------"
--- crm.rb
+++ crm.rb
@@ -1,39 +1,78 @@
+require "./contact.rb"
+
 class CRM
 
-  def initialize
-
-  end
+  # def initialize not used.
 
   def main_menu
-
+    while true # repeat indefinitely
+      print_main_menu
+      user_selected = gets.to_i
+      call_option(user_selected)
+    end
   end
 
   def print_main_menu
-
+    puts '[1] Add a new contact'
+    puts '[2] Modify an existing contact'
+    puts '[3] Delete a contact'
+    puts '[4] Display all the contacts'
+    puts '[5] Search by attribute'
+    puts '[6] Exit'
+    puts 'Enter a number: '
   end
 
-  def call_option
-
+  def call_option(user_selected)
+    case user_selected
+    when 1 then add_new_contact
+    when 2 then modify_existing_contact
+    # Finish off the rest for 3 through 6
+    # To be clear, the methods add_new_contact and modify_existing_contact
+    # haven't been implemented yet
+    end
   end
 
   def add_new_contact
-
+    puts "Enter a first name:"
+    print "> "
+    first_name = gets.chomp
+    puts "Enter a last name:"
+    print "> "
+    last_name = gets.chomp
+    puts "Enter a note for this contact:"
+    print "> "
+    note = gets.chomp
+    puts "Enter an email address:"
+    print "> "
+    email = gets.chomp
+    contact = Contact.create(first_name, last_name, email, note)
+    puts "#{first_name} #{last_name} added with the email address of #{email} and a note of #{note}"
+    return contact
   end
 
   def modify_existing_contact
-
+    id = get_id
+    puts "#{Contact.find(id).first_name} #{Contact.find(id).last_name} | #{Contact.find(id).email} | #{Contact.find(id).note}"
+    print_modify_menu
+    user_selected = gets.to_i
+    call_modify_option(user_selected, id)
+    puts "#{Contact.find(id).first_name} #{Contact.find(id).last_name}| #{Contact.find(id).email}| #{Contact.find(id).note}"
   end
 
   def delete_contact
-
+    id = get_id
+    contact = Contact.find(id)
+    contact.delete
+    puts "#{contact.first_name} #{contact.last_name} deleted."
   end
 
   def display_all_contacts
-    
+    puts "Contact List:"
+    puts Contact.contacts
   end
 
   def search_by_attribute
-    
+
   end
 
 
